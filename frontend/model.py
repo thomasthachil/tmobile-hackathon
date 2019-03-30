f21d94d15b04f5447febd9f1bcb2664f656a966a
jjacob35 <jeffrey.jacob@gatech.edu>
2019-03-30 14:53:46 -0400

    added file with data prepopulated for testing

--- frontend/model.py
+++ frontend/model.py
@@ -17,61 +17,68 @@ text_analytics_base_url = "https://westcentralus.api.cognitive.microsoft.com/tex
 
 class StoreDB:
     def __init__(self):
-        self.df = pd.read_json('../datascraper/data.json', dtype=object)
-        self.df = self.df.set_index('name')
-        self.df['agg_sent'] = 0.0
-        self.df['stars'] = 0.0
-        self.df['sentiments'] = None
-        self.df['sentiments'] = self.df['sentiments'].astype(object)
-        self.df['key_phrases_pos'] = None
-        self.df['key_phrases_pos'] = self.df['key_phrases_pos'].astype(object)
-        self.df['key_phrases_neg'] = None
-        self.df['key_phrases_neg'] = self.df['key_phrases_neg'].astype(object)
-        for index, row in self.df.iterrows():
-            idnum = 0
-            docDic = []
-            fiveRate = []
-            for rev in self.df.loc[index]['reviews']:
-                docDic.append({'id':idnum, 'language':'en', 'text': rev[0]})
-                fiveRate.append(float(rev[1]))
-                idnum += 1
-            self.df.at[index, 'stars'] = sum(fiveRate) / float(len(fiveRate))
-            documents = {'documents' : docDic}
-            headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
-            sentiment_api_url = text_analytics_base_url + "sentiment"
-            response  = requests.post(sentiment_api_url, headers=headers, json=documents)
-            sentiments = response.json()
-            sent_scores = []
-            pos_docs = []
-            neg_docs = []
-            try:
-                for doc in sentiments['documents']:
-                    val = doc['score']
-                    if val > 0.5:
-                        pos_docs.append(doc['id'])
-                    else:
-                        neg_docs.append(doc['id'])
-                    sent_scores.append(doc['score'])
-            except:
-                print(documents)
-                print(sentiments)
-            self.df.at[index, 'agg_sent'] = sum(sent_scores) / float(len(sent_scores))
-            self.df.at[index, 'sentiments'] = sentiments['documents']
+        try:
+            print("Found File")
+            self.df = pd.read_json('fullformJSON.json')
+            print(self.df)
+        except:
+            self.df = pd.read_json('../datascraper/data.json', dtype=object)
+            self.df = self.df.set_index('name')
+            self.df['agg_sent'] = 0.0
+            self.df['stars'] = 0.0
+            self.df['sentiments'] = None
+            self.df['sentiments'] = self.df['sentiments'].astype(object)
+            self.df['key_phrases_pos'] = None
+            self.df['key_phrases_pos'] = self.df['key_phrases_pos'].astype(object)
+            self.df['key_phrases_neg'] = None
+            self.df['key_phrases_neg'] = self.df['key_phrases_neg'].astype(object)
+            for index, row in self.df.iterrows():
+                idnum = 0
+                docDic = []
+                fiveRate = []
+                for rev in self.df.loc[index]['reviews']:
+                    docDic.append({'id':idnum, 'language':'en', 'text': rev[0]})
+                    fiveRate.append(float(rev[1]))
+                    idnum += 1
+                self.df.at[index, 'stars'] = sum(fiveRate) / float(len(fiveRate))
+                documents = {'documents' : docDic}
+                headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
+                sentiment_api_url = text_analytics_base_url + "sentiment"
+                response  = requests.post(sentiment_api_url, headers=headers, json=documents)
+                sentiments = response.json()
+                sent_scores = []
+                pos_docs = []
+                neg_docs = []
+                try:
+                    for doc in sentiments['documents']:
+                        val = doc['score']
+                        if val > 0.5:
+                            pos_docs.append(doc['id'])
+                        else:
+                            neg_docs.append(doc['id'])
+                        sent_scores.append(doc['score'])
+                except:
+                    print(documents)
+                    print(sentiments)
+                self.df.at[index, 'agg_sent'] = sum(sent_scores) / float(len(sent_scores))
+                self.df.at[index, 'sentiments'] = sentiments['documents']
 
-            key_phrase_api_url = text_analytics_base_url + "keyPhrases"
-            headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
-            response  = requests.post(key_phrase_api_url, headers=headers, json=documents)
-            key_phrases = response.json()
-            pos_phrases = []
-            neg_phrases = []
-            for doc in key_phrases['documents']:
-                if doc['id'] in pos_docs:
-                    pos_phrases.append(doc)
-                elif doc['id'] in neg_docs:
-                    neg_phrases.append(doc)
+                key_phrase_api_url = text_analytics_base_url + "keyPhrases"
+                headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
+                response  = requests.post(key_phrase_api_url, headers=headers, json=documents)
+                key_phrases = response.json()
+                pos_phrases = []
+                neg_phrases = []
+                for doc in key_phrases['documents']:
+                    if doc['id'] in pos_docs:
+                        pos_phrases.append(doc)
+                    elif doc['id'] in neg_docs:
+                        neg_phrases.append(doc)
 
-            self.df.at[index, 'key_phrases_pos'] = pos_phrases
-            self.df.at[index, 'key_phrases_neg'] = neg_phrases
+                self.df.at[index, 'key_phrases_pos'] = pos_phrases
+                self.df.at[index, 'key_phrases_neg'] = neg_phrases
+            
+            self.df.to_json('fullformJSON.json')
             
 
     def key_phrases_pos(self, store):
@@ -144,4 +151,3 @@ class StoreDB:
 
 if __name__ == "__main__":
     DB = StoreDB()
-    print(DB.zipcode_lookup(30309))

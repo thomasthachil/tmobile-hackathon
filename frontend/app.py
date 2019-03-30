21b411dd670876d8faf9996cc2685b2e85a41035
Thomas Thachil <github@thomasthachil.com>
2019-03-30 10:56:30 -0400

    front-end changes

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,8 @@
+
 from flask import Flask, render_template, request, jsonify
+# from flask_wtf import FlaskForm
+# from wtforms import IntegerField, validators, StringField
+# from wtforms_components import TimeField
 import json
 import os
 import nltk
@@ -13,20 +17,46 @@ import seaborn as sns
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisasupersecretkey!'
-subscription_key = os.environ['MS_KEY']
+subscription_key = 'c1005ffa18cc42ddbf916d2483bb2dd2'  # os.environ['MS_KEY']
 assert subscription_key
 text_analytics_base_url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/"
 df = pd.read_json('../datascraper/data.json')
 
 # TODO: Have each method be self contained, calling only the other methods it needs.
 
+
+# class HealthForm(FlaskForm):
+#     zipcode = IntegerField('Zip Code', validators=[
+#                            validators.input_required(), validators.Length(min=5, max=5)])
+#     radius = IntegerField('Radius', validators=[validators.input_required()])
+
+
+# class MeetingForm(FlaskForm):
+#     customerName = StringField('Customer Name', validators=[
+#                                validators.input_required()])
+#     time = TimeField('Appointment Time', validators=[
+#                      validators.input_required()])
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/home')
-def home():
-    return render_template('index.html')
+
+@app.route('/find-review', methods=['GET', 'POST'])
+def findReview():
+    if request.method == 'POST':
+        zipcode = request.form['zipcode']
+        radius = request.form['radius']
+    return render_template('findReview.html')
+
+
+@app.route('/find-customer', methods=['GET', 'POST'])
+def findCustomer():
+    # meeting = MeetingForm()
+
+    return render_template('findCustomer.html')
+
 
 @app.route('/review')
 def review():
@@ -41,12 +71,13 @@ def storeRating():
     idnum = 1
     docDic = []
     ratings = []
+
+    # rev 0 is review
+    # rev1 is score otu of 5
+    #rev2 is date
+
     for rev in revoi:
         # docDic.append({'id':idnum, 'language':'en', 'text': rev[0]})
         ratings.append(rev[1])
         # idnum += 1
     return jsonify(rating=(sum(ratings) / float(len(ratings))))
-
-    
-
-    
